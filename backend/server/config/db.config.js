07bceb74a05a564bcbb5d12fab3b313397068fea
Jake <jakebailes76@yahoo.com>
2024-01-30 15:35:54 -0500

    register page fix

--- backend/server/config/db.config.js
+++ backend/server/config/db.config.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
 
-module.exports = () => {
-    const databaseParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-    try{
-        mongoose.connect(process.env.DB_URL)
-        console.log("The backend has connected to the MongoDB database.")
-    } catch(error){
-        console.log(`${error} could not connect`)
-    }
-}
+module.exports = async () => {
+  const databaseParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 
+  try {
+    // Check if process.env.DB_URL is defined before attempting to connect
+    if (!process.env.DB_URL) {
+      throw new Error('MongoDB connection URI is not defined.')
+    }
+
+    await mongoose.connect(process.env.DB_URL, databaseParams)
+    console.log('The backend has connected to the MongoDB database.')
+  } catch (error) {
+    console.error(`Could not connect to MongoDB: ${error.message}`)
+  }
+}
