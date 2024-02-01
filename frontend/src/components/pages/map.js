b761ed76f70abe494e4cf8da1c314214c5c7ef7d
Jake <jakebailes76@yahoo.com>
2024-02-01 18:12:10 -0500

    Live-Tracker filters

--- frontend/src/components/pages/map.js
+++ frontend/src/components/pages/map.js
@@ -1,61 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import axios from "axios";
-import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
+import axios from 'axios'
+import 'leaflet/dist/leaflet.css'
 
-const LeafletMap = () => {
-  const [trainData, setTrainData] = useState([]);
+const LeafletMap = ({ routeTypeFilter }) => {
+  const [trainData, setTrainData] = useState([])
 
   useEffect(() => {
     // Fetch live train data from the MBTA API
     const fetchTrainData = async () => {
       try {
-        const response = await axios.get("https://api-v3.mbta.com/vehicles");
-        setTrainData(response.data.data);
+        if (routeTypeFilter !== undefined) {
+          const response = await axios.get(`https://api-v3.mbta.com/vehicles?filter%5Broute_type%5D=${routeTypeFilter}`)
+          console.log('route type filter: ' + routeTypeFilter)
+          setTrainData(response.data.data)
+        }
       } catch (error) {
-        console.error("Error fetching train data:", error);
+        console.error('Error fetching train data:', error)
       }
-    };
+    }
 
     // Fetch train data every 5 seconds (adjust as needed)
     const intervalId = setInterval(() => {
-      fetchTrainData();
-    }, 2000);
+      fetchTrainData()
+    }, 10000)
 
     // Initial fetch
-    fetchTrainData();
+    fetchTrainData()
 
     // Cleanup interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []);
+    return () => clearInterval(intervalId)
+  }, [routeTypeFilter])
 
   // Define subway icon
   const subwayIcon = new L.Icon({
-    iconUrl: process.env.PUBLIC_URL + "/train.png",
+    iconUrl: process.env.PUBLIC_URL + '/train.png',
     iconSize: [32, 32],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
-  });
+  })
 
   return (
     <MapContainer
-      style={{ height: "500px", width: "100%" }}
+      style={{ height: '500px', width: '100%' }}
       center={[42.3601, -71.0589]} // Set a default center
       zoom={13}
     >
       <TileLayer
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
       {/* Map through trainData to create markers */}
-      {trainData.map((train) => (
-        <Marker
-          key={train.id}
-          position={[train.attributes.latitude, train.attributes.longitude]}
-          icon={subwayIcon}
-        >
+      {trainData.map(train => (
+        <Marker key={train.id} position={[train.attributes.latitude, train.attributes.longitude]} icon={subwayIcon}>
           <Popup>
             Train ID: {train.id}
             <br />
@@ -64,7 +63,7 @@ const LeafletMap = () => {
         </Marker>
       ))}
     </MapContainer>
-  );
-};
+  )
+}
 
-export default LeafletMap;
+export default LeafletMap
--- frontend/src/components/pages/mbtaTracker.js
+++ frontend/src/components/pages/mbtaTracker.js
@@ -1,21 +1,33 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import LeafletMap from "./map"; // Replace with the actual path to your LeafletMap component
+import React, { useEffect, useState } from 'react'
+import { Card } from 'react-bootstrap'
+import LeafletMap from './map' // Replace with the actual path to your LeafletMap component
 
 const Trackerpage = () => {
+  const [routeTypeFilter, setRouteTypeFilter] = useState(null)
+
+  const handleFilterClick = routeType => {
+    setRouteTypeFilter(routeType)
+    console.log('Route Type: ' + routeType)
+  }
+
+  console.log('Current Route Type Filter: ', routeTypeFilter) // Log the current routeTypeFilter
+
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <Card
-        style={{ width: "80%", height: "80%" }}
-        className="text-center bg-dark shadow-xl rounded border"
-      >
-        <Card.Body className="d-flex flex-column align-items-center justify-content-center text-light">
+    <div className='d-flex justify-content-center align-items-center vh-100'>
+      <Card style={{ width: '80%', height: '80%' }} className='text-center bg-dark shadow-xl rounded border'>
+        <div className='mt-4'>
+          <button onClick={() => handleFilterClick(0)}>Tram</button>
+          <button onClick={() => handleFilterClick(1)}>Subway</button>
+          <button onClick={() => handleFilterClick(2)}>Rail</button>
+          {/* Add more buttons for other route types if needed */}
+        </div>
+        <Card.Body className='d-flex flex-column align-items-center justify-content-center text-light'>
           <Card.Title>Live Tracker</Card.Title>
-          <LeafletMap />
+          <LeafletMap routeTypeFilter={routeTypeFilter} />
         </Card.Body>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default Trackerpage;
+export default Trackerpage
