00fd6c6c66e6636d47addf8d22cb0ed37dbf96c6
Jacob Bailes <jakebailes@Jacobs-MacBook-Pro.local>
2024-04-11 12:34:34 -0400

    sign-in label color to white

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,64 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import getUserInfo from '../../utilities/decodeJwt'
-import { Card, Container } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import getUserInfo from "../../utilities/decodeJwt";
+import { Card, Container } from "react-bootstrap";
 const HomePage = () => {
-  const [user, setUser] = useState({})
-  const navigate = useNavigate()
-  const handleClick = e => {
-    e.preventDefault()
-    localStorage.removeItem('accessToken')
-    return navigate('/')
-  }
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("accessToken");
+    return navigate("/");
+  };
   const handleLogout = async () => {
-    localStorage.clear()
-    navigate('/')
-  }
+    localStorage.clear();
+    navigate("/");
+  };
 
   useEffect(() => {
-    setUser(getUserInfo())
-  }, [])
+    setUser(getUserInfo());
+  }, []);
 
   if (!user)
     return (
-      <div className='d-flex align-items-center justify-content-center vh-100'>
+      <div className="d-flex align-items-center justify-content-center vh-100">
         <h4>Log in to view this page.</h4>
       </div>
-    )
-  const { id, email, username, password } = user
+    );
+  const { id, email, username, password } = user;
   return (
-    <Container>
-      <Card className='d-flex align-items-center justify-items-center p-4 m-4'>
+    <Container style={{ padding: "4px", marginLeft: "13%" }}>
+      <Card className="d-flex align-items-center justify-items-center p-4 m-4">
         <Card.Body>
           <>
             <div>
-              <h3 className='text-center mb-4'>
+              <h3 className="text-center mb-4">
                 Welcome
-                <span className='username'> @{username}</span>
+                <span className="username"> @{username}</span>
               </h3>
               <h3>
                 Your userId in mongo db is
-                <span className='userId'> {id}</span>
+                <span className="userId"> {id}</span>
               </h3>
               <h3>
                 Your registered email is
-                <span className='email'> {email}</span>
+                <span className="email"> {email}</span>
               </h3>
               <h3>
                 Your password is
                 <div>
-                  <span className='password'> {password} ( hashed )</span>
+                  <span className="password"> {password} ( hashed )</span>
                 </div>
               </h3>
             </div>
-            <div className='text-center mt-4'>
-              <button onClick={e => handleClick(e)}>Log Out</button>
+            <div className="text-center mt-4">
+              <button onClick={(e) => handleClick(e)}>Log Out</button>
             </div>
           </>
         </Card.Body>
       </Card>
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- frontend/src/components/pages/loginPage.js
+++ frontend/src/components/pages/loginPage.js
@@ -1,108 +1,134 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import axios from 'axios'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import getUserInfo from '../../utilities/decodeJwt'
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import getUserInfo from "../../utilities/decodeJwt";
 
-const PRIMARY_COLOR = 'red'
-const SECONDARY_COLOR = '#0c0c1f'
-const url = 'http://localhost:8081/user/login'
+const PRIMARY_COLOR = "white";
+const SECONDARY_COLOR = "#0c0c1f";
+const url = "http://localhost:8081/user/login";
 
 const Login = () => {
-  const [user, setUser] = useState(null)
-  const [data, setData] = useState({ username: '', password: '' })
-  const [error, setError] = useState('')
-  const [bgColor, setBgColor] = useState(SECONDARY_COLOR)
-  const navigate = useNavigate()
+  const [user, setUser] = useState(null);
+  const [data, setData] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+  const [bgColor, setBgColor] = useState(SECONDARY_COLOR);
+  const navigate = useNavigate();
 
   let labelStyling = {
     color: PRIMARY_COLOR,
-    fontWeight: 'bold',
-    textDecoration: 'none',
-    textShadow: '2px 2px 4px #000000'
-  }
-  let backgroundStyling = { 
+    fontWeight: "",
+    textDecoration: "none",
+    textShadow: "2px 2px 4px #000000",
+  };
+  let backgroundStyling = {
     background: bgColor,
-    backgroundImage: 'url(https://bdc2020.o0bc.com/wp-content/uploads/2019/09/orangelinecar-768x432.jpeg?width=800)', 
-    backgroundSize: 'cover', 
-    backgroundPosition: 'center', 
-  }
+    backgroundImage:
+      "url(https://bdc2020.o0bc.com/wp-content/uploads/2019/09/orangelinecar-768x432.jpeg?width=800)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
 
   let buttonStyling = {
     background: PRIMARY_COLOR,
-    borderStyle: 'none',
+    borderStyle: "none",
     color: bgColor,
-  }
+  };
 
   const handleChange = ({ currentTarget: input }) => {
-    setData({ ...data, [input.name]: input.value })
-  }
+    setData({ ...data, [input.name]: input.value });
+  };
 
   useEffect(() => {
-    const obj = getUserInfo(user)
-    setUser(obj)
+    const obj = getUserInfo(user);
+    setUser(obj);
 
     if (user) {
-      navigate('/home')
+      navigate("/home");
     }
-  }, [user, navigate])
+  }, [user, navigate]);
 
-  const handleSubmit = async e => {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      let audio = new Audio('mixkit-train-door-close-1638.wav'); 
-      audio.play()
-      const { data: res } = await axios.post(url, data)
-      const { accessToken } = res
+      let audio = new Audio("mixkit-train-door-close-1638.wav");
+      audio.play();
+      const { data: res } = await axios.post(url, data);
+      const { accessToken } = res;
       //store token in localStorage
-      localStorage.setItem('accessToken', accessToken)
-      navigate('/home')
+      localStorage.setItem("accessToken", accessToken);
+      navigate("/home");
     } catch (error) {
-      if (error.response && error.response.status >= 400 && error.response.status <= 500) {
-        setError(error.response.data.message)
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
       }
     }
-  }
+  };
 
   if (user) {
-    navigate('/home')
-    return
+    navigate("/home");
+    return;
   }
 
   return (
     <>
-      <section className='vh-100'>
-        <div className='container-fluid h-custom vh-100'>
-          <div className='row d-flex justify-content-center align-items-center h-100 ' style={backgroundStyling}>
-            <div className='col-md-8 col-lg-6 col-xl-4 offset-xl-1'>
+      <section className="vh-100">
+        <div className="container-fluid h-custom vh-100">
+          <div
+            className="row d-flex justify-content-center align-items-center h-100 "
+            style={backgroundStyling}
+          >
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
               <Form>
-                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label style={labelStyling}>Username</Form.Label>
-                  <Form.Control type='username' name='username' onChange={handleChange} placeholder='Enter username' />
-                  <Form.Text className='text-muted'>We just might sell your data</Form.Text>
+                  <Form.Control
+                    type="username"
+                    name="username"
+                    onChange={handleChange}
+                    placeholder="Enter username"
+                  />
+                  <Form.Text className="text-muted">
+                    We just might sell your data
+                  </Form.Text>
                 </Form.Group>
-                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label style={labelStyling}>Password</Form.Label>
-                  <Form.Control type='password' name='password' placeholder='Password' onChange={handleChange} />
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
                 </Form.Group>
-                <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-                  <Form.Text className='text-muted pt-1'>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Text className="text-muted pt-1">
                     Dont have an account?
                     <span>
-                      <Link to='/signup' style={labelStyling}>
-                        {' '}
+                      <Link to="/signup" style={labelStyling}>
+                        {" "}
                         Sign up
                       </Link>
                     </span>
                   </Form.Text>
                 </Form.Group>
                 {error && (
-                  <div style={labelStyling} className='pt-3'>
+                  <div style={labelStyling} className="pt-3">
                     {error}
                   </div>
                 )}
-                <Button variant='primary' type='submit' onClick={handleSubmit} style={buttonStyling} className='mt-2'>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={handleSubmit}
+                  style={buttonStyling}
+                  className="mt-2"
+                >
                   Log In
                 </Button>
               </Form>
@@ -111,7 +137,7 @@ const Login = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
